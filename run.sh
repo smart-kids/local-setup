0b0c37ce47e1500eff84b5cf5696c566a83cb6c4
BransonGitomeh <sirbranson67@gmail.com>
2022-01-12 21:17:20 +0300

    innitial commit

---
+++ run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -xe
+
+# Define variables
+
+DAY=`date +%u`
+ARCHIVE=cointelegraph-$DAY.sql.gz
+DUMP=cointelegraph-$DAY.sql
+
+repositories=(
+    # needs duke and node-graph added
+    git@github.com:smart-kids/graph.git
+    git@github.com:smart-kids/admin.git
+    git@github.com:smart-kids/parent-and-bus-agent-app.git
+)
+
+clone_repositories () {
+    echo "Cloning necessary repositories..."
+
+    mkdir -p ../code
+    for i in "${repositories[@]}"
+    do
+        if [ ! -d "../code/$i" ] ; then
+            pwd
+            # git clone "$i" "../code/$i"
+        fi
+        
+    done
+
+    cd ../local-setup
+}
+
+build_and_run () {
+   ls
+   docker-compose up
+}
+
+# Clone all the repos to "../code/"
+clone_repositories
+
+# Build docker images and run containers
+build_and_run
